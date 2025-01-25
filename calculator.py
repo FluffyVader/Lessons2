85a0b73d9525a380a209b76ff3f88531e8f622b7
FluffyVader <wewilllearnthis@gmail.com>
2025-01-25 14:58:52 +0000

    I made the calculator more compact and added 2 more buttons.

--- calculator.py
+++ calculator.py
@@ -2,17 +2,23 @@ import tkinter
 from tkinter import *
 Window = Tk()
 
+#init block
+initial_text_field_X = 100
+initial_text_field_Y = 100
+backspace_symbol = "BSP"
 button_size = 100
 button_colour = "Black"
 text_color = "Yellow"
-width_of_screen = 2000
-height_of_screen = 2000
+width_of_screen = 800
+height_of_screen = 800
 bgcolor = "White"
 init_button_x = 100
-init_button_y = 100
+init_button_y = 250
 button_margin_x = 10
 button_margin_y = 10
-
+text_to_parse = ""
+screen = Canvas(Window,width = width_of_screen, height = height_of_screen, bg = bgcolor)
+text_view = screen.create_text(initial_text_field_X,    initial_text_field_Y,   font = "Arial 28",  text = text_to_parse,   fill = "Black")
 
 Window.title("Calculator")
 
@@ -38,15 +44,28 @@ def Sum(a,b):
 
 def draw_text_field(text_param):
     global text_view
+
     screen.delete(text_view)
-    text_view = screen.create_text(init_button_x + button_size, init_button_y+button_size*5,font = "Arial 28", text = text_param, fill = "black")
+    text_view = screen.create_text(0 + button_size, button_size,font = "Arial 28", text = text_param, fill = "black" ,anchor=tkinter.NW)
     
 
 def on_button_clicked(button_text):
+    if button_text == backspace_symbol:
+        on_back_space_pressed()
+        return
+
     global text_view, text_to_parse
 
     text_to_parse += button_text
     draw_text_field(text_to_parse)
+
+def on_back_space_pressed():
+    global text_to_parse
+
+    text_to_parse = text_to_parse[:-1]
+    draw_text_field(text_to_parse)
+
+
 
 def apply_buttons_horizontal_layout():
     draw_button(init_button_x,                                          init_button_y,    button_size,    button_size,    "1",    button_colour,  text_color)
@@ -67,19 +86,32 @@ def apply_buttons_horizontal_layout():
     draw_button_equals (init_button_x+button_size*15+button_margin_x*16,        init_button_y,    button_size,    button_size,    "=",    button_colour,  text_color)
 
 def apply_buttons_2d_layout():
-    draw_button(init_button_x+button_size*3+button_margin_x*3,          init_button_y,    button_size,    button_size,    "4",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*4+button_margin_x*4,          init_button_y,    button_size,    button_size,    "5",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*5+button_margin_x*5,          init_button_y,    button_size,    button_size,    "6",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*6+button_margin_x*6,          init_button_y,    button_size,    button_size,    "7",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*7+button_margin_x*7,          init_button_y,    button_size,    button_size,    "8",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*8+button_margin_x*8,          init_button_y,    button_size,    button_size,    "9",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*9+button_margin_x*9,          init_button_y,    button_size,    button_size,    "0",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*10+button_margin_x*10,        init_button_y,    button_size,    button_size,    "+",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*11+button_margin_x*11,        init_button_y,    button_size,    button_size,    "-",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*12+button_margin_x*13,        init_button_y,    button_size,    button_size,    "/",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*13+button_margin_x*14,        init_button_y,    button_size,    button_size,    "*",    button_colour,  text_color)
-    draw_button(init_button_x+button_size*14+button_margin_x*15,        init_button_y,    button_size,    button_size,    "%",    button_colour,  text_color)
-    draw_button_equals (init_button_x+button_size*15+button_margin_x*16,        init_button_y,    button_size,    button_size,    "=",    button_colour,  text_color)
+
+    draw_button(init_button_x,                                                  init_button_y,    button_size,    button_size,    "7",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*1+button_margin_x*1,                  init_button_y,    button_size,    button_size,    "8",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*2+button_margin_x*2,                  init_button_y,    button_size,    button_size,    "9",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*3+button_margin_x*4,                  init_button_y,    button_size,    button_size,    "*",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*4+button_margin_x*5,                  init_button_y,    button_size,    button_size,    "/",    button_colour,  text_color)
+    draw_button(init_button_x,                                                  init_button_y+button_size+button_margin_y,    button_size,    button_size,    "4",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*1+button_margin_x*1,                  init_button_y+button_size+button_margin_y,    button_size,    button_size,    "5",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*2+button_margin_x*2,                  init_button_y+button_size+button_margin_y,    button_size,    button_size,    "6",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*3+button_margin_x*4,                  init_button_y+button_size+button_margin_y,    button_size,    button_size,    "-",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*4+button_margin_x*5,                  init_button_y+button_size+button_margin_y,    button_size,    button_size,    "+",    button_colour,  text_color)
+    draw_button(init_button_x,                                                  init_button_y+button_size*2+button_margin_y*2,    button_size,    button_size,    "1",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*1+button_margin_x*1,                  init_button_y+button_size*2+button_margin_y*2,    button_size,    button_size,    "2",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*2+button_margin_x*2,                  init_button_y+button_size*2+button_margin_y*2,    button_size,    button_size,    "3",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*3+button_margin_x*4,                  init_button_y+button_size*2+button_margin_y*2,    button_size,    button_size,    "%",    button_colour,  text_color)
+    draw_button(init_button_x+button_size*4+button_margin_x*5,                  init_button_y+button_size*2+button_margin_y*2,    button_size,    button_size,    backspace_symbol,    button_colour,  text_color)
+    draw_button(init_button_x+button_size*1+button_margin_x*1,                  init_button_y+button_size*3+button_margin_y*3,    button_size,    button_size,    "0",    button_colour,  text_color)
+    
+    draw_button_equals(init_button_x+button_size*4+button_margin_x*5,                  init_button_y+button_size*3+button_margin_y*3,    button_size,    button_size,    "=",    button_colour,  text_color)
+    
+#    draw_button(init_button_x+button_size*10+button_margin_x*10,                init_button_y,    button_size,    button_size,    "+",    button_colour,  text_color)
+#    draw_button(init_button_x+button_size*11+button_margin_x*11,                init_button_y,    button_size,    button_size,    "-",    button_colour,  text_color)
+#    draw_button(init_button_x+button_size*12+button_margin_x*13,                init_button_y,    button_size,    button_size,    "/",    button_colour,  text_color)
+#    draw_button(init_button_x+button_size*13+button_margin_x*14,                init_button_y,    button_size,    button_size,    "*",    button_colour,  text_color)
+#    draw_button(init_button_x+button_size*14+button_margin_x*15,                init_button_y,    button_size,    button_size,    "%",    button_colour,  text_color)
+#    draw_button_equals (init_button_x+button_size*15+button_margin_x*16,        init_button_y,    button_size,    button_size,    "=",    button_colour,  text_color)
 
 
 def draw_button(x, y, width, height, button_text, button_colour, text_color):
@@ -114,24 +146,12 @@ def draw_button_equals(x, y, width, height, button_text, button_colour, text_col
 
 
 def on_button_clicked_equals():
-    global text_view
+    global text_view, text_to_parse
     screen.delete(text_view)
+    text_to_parse = ""
 #    answer = culculate(text)
 
 
-
-
-
-text_to_parse = ""
-width_of_screen = 2000
-height_of_screen = 2000
-bgcolor = "White"
-init_button_x = 100
-init_button_y = 100
-button_margin_x = 10
-button_margin_y = 10
-screen = Canvas(Window,width = width_of_screen, height = height_of_screen, bg = bgcolor)
-text_view = screen.create_text(init_button_x+button_size,init_button_y+button_size*5,font = "Arial 28",text = text_to_parse, fill = "yellow")
 
 screen.pack()
 
