2ab00bbedcfb049b87957a1b0af48696a8055d4b
FluffyVader <wewilllearnthis@gmail.com>
2025-03-15 18:05:18 +0000

    Ive made a puzzle and now need to solve it too see the ending if the code works or not.

--- puzzle.py
+++ puzzle.py
@@ -13,44 +13,73 @@ sc.clearscreen()
 sc.bgcolor("black")
 sc.setup(width = width_, height = height_)
 sc.title("puzzle")
-puzzles = []
-number_of_puzzles = []
+
+def update_puzzle ():
+    tur.clear()
+    draw_puzzles()
+ #   sc.update()
 
 
+def generate_puzzle ():
+    numbers = list ( range ( 1 , max_number)) + [ None ]
+    random.shuffle(numbers)
+    return [numbers[i:i + screen_size] for i in range ( 0 , len (numbers), screen_size)]
 
 
+def move_left():      
+    y, x =  find_empty_tile()
+    if x > 0:
 
-def move_up():      ###
-    if missing_puzzle != 1 and 4 and 7:
-        global puzzles
-        y = puzzles[missing_puzzle - 1].ycor() + puzzle_size
-        puzzles[missing_puzzle - 1].sety(y)
-#def move_down():    ###
-#    y = paddA.ycor() - 20 
-#    if y < -160:
-#        y = -160 
-#    paddA.sety(y) 
-#def move_left():      ###
-#   y = paddB.ycor() + 20 
-#    if y > 160:
-#       y = 160 
-#   paddB.sety(y) 
-#def move_right():    ####
-#    y = paddB.ycor() - 20 
-#    if y < -160:
-#        y = -160 
-#   paddB.sety(y)
+        value = puzzles[x-1][y]
+        puzzles[x][y] = value
+        puzzles[x-1][y] = None
+        draw_puzzle_title_none(x-2, y-1)
+        draw_puzzle_title(x-1,y-1,value)
 
+def move_right():
+    y, x =  find_empty_tile()
+    if x < 2:
 
+        value = puzzles[x+1][y]
+        puzzles[x][y] = value
+        puzzles[x+1][y] = None
+        draw_puzzle_title_none(x,y-1)
+        draw_puzzle_title(x-1,y-1,value)
+def move_up():      
+    y, x =  find_empty_tile()
+    if y > 0:
 
+        value = puzzles[x][y-1]
+        puzzles[x][y] = value
+        puzzles[x][y-1] = None
+        draw_puzzle_title_none(x-1,y-2)
+        draw_puzzle_title(x-1,y-1,value)
+
+def move_down():      
+    y, x =  find_empty_tile()
+    if y < 2:
+
+        value = puzzles[x][y+1]
+        puzzles[x][y] = value
+        puzzles[x][y+1] = None
+        draw_puzzle_title_none(x-1, y)
+        draw_puzzle_title(x-1,y-1,value)
 sc.listen() 
-sc.onkey(move_up, "Up") 
-#sc.onkey(move_down, "Down") 
-#sc.onkey(move_right, "Right")
-#sc.onkey(move_left, "left")
+sc.onkey(move_left, "a") 
+sc.onkey(move_right, "d") 
+sc.onkey(move_up, "w")
+sc.onkey(move_down, "s")
 
-
-
+def finnish_game():
+    number = 1
+    for i in range(0,3,1):
+        for j in range(0,3,1):
+            if puzzles[i][j] != number:
+                return
+    tur.goto(0,0)
+    
+    tur.pendown()
+    tur.write("You win",align = "center", font = ("Arial", 50 , "normal "))
 
 # draws a big white puzzle canvas
 def draw_puzzle():
@@ -65,50 +94,76 @@ def draw_puzzle():
         tur.left(90)
     tur.end_fill()
 
+def find_empty_tile():
+    for y in range (screen_size):
+        for x in range (screen_size):
+            if puzzles[y][x] is None :
+                return x, y
+
+
 #draws a puzzle tile with a number on it on the puzzle canvas
-def draw_puzzle_tile(x,y,number):
+def draw_puzzle_title(x,y,number):
     global puzzle, puzzles
     startX = 0-x*puzzle_size
     startY = y*puzzle_size  
-    puzzle = turtle.Turtle()
-    puzzle.penup()
-    puzzle.goto(startX,startY)
-    puzzle.pendown()
-    puzzle.color("grey")
-    puzzle.begin_fill()
+    tur = turtle.Turtle()
+    tur.penup()
+    tur.goto(startX,startY)
+    tur.pendown()
+    tur.color("grey")
+    tur.begin_fill()
     for i in range(4):
-        puzzle.forward(puzzle_size)
-        puzzle.left(90)
-    puzzle.end_fill()
-    puzzle.color("red")
-    puzzle.hideturtle()
-    puzzle.end_fill()
-    puzzle.penup()
+        tur.forward(puzzle_size)
+        tur.left(90)
+    tur.end_fill()
+    tur.color("red")
+    tur.hideturtle()
+    tur.end_fill()
+    tur.penup()
     
-    puzzle.goto(startX+puzzle_size/2,startY+puzzle_size/2-50)
+    tur.goto(startX+puzzle_size/2,startY+puzzle_size/2-50)
     
-    puzzle.pendown()
-    puzzle.write(str(number), align = "center", font = ("Arial", 50 , "normal "))
-    puzzles += [puzzle]
+    tur.pendown()
+    tur.write(str(number), align = "center", font = ("Arial", 50 , "normal "))
+
+def draw_puzzles():
+    for i in range(int(max_number ** 0.5)):
+
+        for j in range(int(max_number ** 0.5)):
+            if puzzles[i][j]!= None:
+                
+                draw_puzzle_title(i-1,j-1,puzzles[i][j])
+            else:
+                draw_puzzle_title_none(i-1,j-1)
+                
+def draw_puzzle_title_none(x,y):
+    global puzzle, puzzles
+    startX = 0-x*puzzle_size
+    startY = y*puzzle_size  
+    tur = turtle.Turtle()
+    tur.penup()
+    tur.goto(startX,startY)
+    tur.pendown()
+    tur.color("white")
+    tur.begin_fill()
+    for i in range(4):
+        tur.forward(puzzle_size)
+        tur.left(90)
+    tur.end_fill()
+    tur.color("red")
+    tur.hideturtle()
+    tur.end_fill()
+    tur.penup()
     
-#def move_left(list0):
+                #def move_left(list0):
 # the program starts in the line below.
 draw_puzzle()
 
 
+puzzles = generate_puzzle()
 
+draw_puzzles()
 
-
-number = 0
-for i in range(int(max_number ** 0.5)):
-
-    for j in range(int(max_number ** 0.5)):
-        if number < max_number -1:
-            
-            draw_puzzle_tile(i-1,j-1,random_var[number])
-            number += 1 
-
-
-
+print(puzzles)
 
 sc.mainloop()
